8b94842bfd46c718cb10403ca3b7904511e1162c
Q4 <64757418+Quantity-4@users.noreply.github.com>
2023-03-31 15:43:12 +0100

    Clear login/error messages. Fixed Comments
    Commenting was the main focus for better documentation.
    
    Login messages are now also clearer - environment variable changes too.

--- src/lib/server/auth.js
+++ src/lib/server/auth.js
@@ -1,5 +1,13 @@
 import bcrypt from "bcrypt";
 
+/**
+ * verifyPassword function takes a plain-text password and a hashed password, and checks if they match.
+ *
+ * @param {string} password - The plain-text password provided by the user.
+ * @param {string} passwordHash - The hashed password stored in the database.
+ * @returns {Promise<boolean>} - Returns true if the plain-text password matches the hashed password, otherwise false.
+ * @throws {Error} If there is an error during the password comparison process.
+ */
 export async function verifyPassword(password, passwordHash) {
   try {
     return await bcrypt.compare(password, passwordHash);
