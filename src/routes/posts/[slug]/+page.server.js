4f68119626edd9d12d786f665f035d6767c32027
Q4 <64757418+Quantity-4@users.noreply.github.com>
2023-05-09 19:44:30 +0100

    Displays comments retrieved from the database

--- src/routes/posts/[slug]/+page.server.js
+++ src/routes/posts/[slug]/+page.server.js
@@ -1,7 +1,7 @@
 import { error } from "@sveltejs/kit";
 import { marked } from "marked";
 import * as Utils from "$lib/server/utils.js";
-import { getPostBySlug } from "$lib/server/db.js";
+import { getPostBySlug, getCommentsByPostId } from "$lib/server/db.js";
 
 /**
  * @typedef { import("$lib/types").Post } Post
@@ -20,6 +20,8 @@ export async function load({ params }) {
   try {
     const post = await getPostBySlug(params.slug);
 
+    const comments = await getCommentsByPostId(post.id);
+
     post.content = await Utils.sanitizeHTML(
       await marked.parse(post.content, {
         async: true,
@@ -29,9 +31,10 @@ export async function load({ params }) {
       );
 
     return {
-      post
+      post,
+      comments
     };
-    
+
   } catch (err) {
     throw error(404, "Not Found");
   }
