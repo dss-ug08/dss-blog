b9d472ebaa38407d93da54a9245c7eb1d39e236e
Jack Mawer <jack@mawersoft.co.uk>
2023-05-09 19:13:13 +0100

    Add Meta, Header & Footer components
    - Add meta component for page title and social embeds
    - Move header and footer to dedicated components
    - Add utils function to get site name, not currently functional

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,24 @@
+<script>
+</script>
+
+<footer>
+  <hr />
+  <small>Copyright &copy; 2023 Jack Mawer, Jen Katuwal, Charis Stanley</small>
+</footer>
+
+<style>
+  footer {
+    width: 100%;
+    margin-right: auto;
+    margin-left: auto;
+    padding: 0 var(--block-spacing-horizontal);
+  }
+
+  footer {
+    padding-top: 1rem;
+  }
+
+  footer > small {
+    color: var(--muted-color);
+  }
+</style>
---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<header>
+  <nav>
+    <ul>
+      <li><a href="/" class="contrast"><strong>dss-blog</strong></a></li>
+      <!---->
+      <li><a href="/posts">Posts</a></li>
+      <li><a href="/search">Search</a></li>
+    </ul>
+    <ul>
+      <!--TODO: display user menu if user is logged in already-->
+      <li><a href="/auth/login" role="button">Log in</a></li>
+      <li><a href="/auth/register" role="button" class="secondary">Register</a>
+      </li>
+    </ul>
+  </nav>
+  <hr />
+</header>
+
+<style>
+  header {
+    width: 100%;
+    margin-right: auto;
+    margin-left: auto;
+    padding: 0 var(--block-spacing-horizontal);
+  }
+</style>
